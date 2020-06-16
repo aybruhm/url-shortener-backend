346013629cfdf50fc24bc7de5ad5793287449883
Israel Abraham <israelvictory87@gmail.com>
2020-06-16 02:33:47 +0100

    Modified the settings config file.

--- shortly/settings.py
+++ shortly/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 django_heroku.settings(locals())

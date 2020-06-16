045af0f64d2a3d00bb209902104e51b611b19644
Israel Abraham <israelvictory87@gmail.com>
2020-06-16 12:41:28 +0100

    Update settings.py

--- shortly/settings.py
+++ shortly/settings.py
@@ -121,13 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-django_heroku.settings(locals())

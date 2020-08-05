597643fee9f4a5e78cd421677464eb5c2949bc69
Israel Abraham <israelvictory87@gmail.com>
2020-08-05 16:25:57 +0100

    CDN: added static files through GitHub pages

--- shortly/settings.py
+++ shortly/settings.py
@@ -123,6 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    'shortly/shortly/static',
+]

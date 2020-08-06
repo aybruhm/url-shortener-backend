7d3daab4b8763e1348cb74bd9928c0d233609cf1
Israel Abraham <israelvictory87@gmail.com>
2020-08-06 10:20:14 +0100

    whitenoise: gzip functionality enabled

--- shortly/settings.py
+++ shortly/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'shortly.urls'
 
 TEMPLATES = [

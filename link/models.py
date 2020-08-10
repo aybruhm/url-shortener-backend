3c1adcbfa08b07f37ad274f22e3f445c299bad64
Israel Abraham <israelvictory87@gmail.com>
2020-08-10 23:54:30 +0100

    Updated settings.py

--- link/models.py
+++ link/models.py
@@ -4,8 +4,9 @@ from django.utils import timezone
 
 class URL(models.Model):
     original_url = models.URLField(max_length=512)
+    short_url = models.CharField(max_length=20)
     visits = models.IntegerField(default=0)
     timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.original_url - self.short_url
+        return self.original_url

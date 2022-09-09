096a8634976c4e322f197f308bf9faa3bd61f18f
Abram <israelvictory87@gmail.com>
2022-09-10 00:59:06 +0100

    Update link/urls.py

--- link/urls.py
+++ link/urls.py
@@ -2,12 +2,16 @@
 from django.urls import path
 
 # Own Imports
-from link.views import ShortenURL, GetOriginalURl
-
+from link.views import (
+    ShortenURL, 
+    GetOriginalURl,
+    URLStats
+)
 
 app_name = "shortener"
 
 urlpatterns = [
     path("shorten/", ShortenURL.as_view(), name="shorten-url"),
     path("<str:token>/", GetOriginalURl.as_view(), name="shorten-url-token"),
+    path("stats/", URLStats.as_view(), name="stats"),
 ]

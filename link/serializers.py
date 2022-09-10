f89d32537c6b5ea3d85e856c83754c408ac6abea
Abram <israelvictory87@gmail.com>
2022-09-10 01:24:18 +0100

    Created get_visits and get_short_url methods

--- link/serializers.py
+++ link/serializers.py
@@ -1,6 +1,9 @@
 # Rest Framework Imports
 from rest_framework import serializers
 
+# Django Imports
+from django.contrib.sites.shortcuts import get_current_site
+
 # Own Imports
 from link.models import URL
 
@@ -11,7 +14,39 @@ class ShortenURLSerializer(serializers.Serializer):
 
 class URLSerializer(serializers.ModelSerializer):
     
+    visits = serializers.SerializerMethodField(read_only=True)
+    short_url = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = URL
         fields = ("original_url", "short_url", "visits", "timestamp")
         
+    def get_visits(self, value):
+        """
+        It takes a short url and returns the number of visits to that url
+        
+        :param value: The value of the URL that the user entered
+        :return: The number of visits to the short URL.
+        """
+        url = URL.objects.get(short_url=value)
+        visits = int(url.visits)
+        return visits
+    
+    def get_short_url(self, value):
+        """
+        If the value is not empty, then return the current site's domain and the value. Otherwise,
+        return an empty string
+        
+        :param value: The value of the field that we are serializing
+        :return: The domain of the current site and the value.
+        """
+
+        request = self.context.get("request")
+        current_site = get_current_site(request)
+        
+        if value:
+            value = current_site.domain + f"/{value}/"
+        else:
+            value = ""
+            
+        return value

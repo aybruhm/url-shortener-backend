277b75fc0199084545153bff66f623f3ca6a605b
Abram <israelvictory87@gmail.com>
2022-09-11 13:03:05 +0100

    Update docs endpoint

--- shortly/urls.py
+++ shortly/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
    
    # api documentation endpoints
    re_path(r'^generate_api_documentation(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   re_path(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
